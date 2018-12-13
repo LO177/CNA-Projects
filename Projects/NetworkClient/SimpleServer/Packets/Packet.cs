c90a68254a464b650385bda4e1c2128cce72c251
LO177 <l017081h@student.staffs.ac.uk>
2018-12-13 16:10:24 +0000

    Created packet project within NetworkClient project

---
+++ Projects/NetworkClient/SimpleServer/Packets/Packet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Packets
+{
+    public enum PacketType
+    {
+        EMPTY, NICKNAME, CHATMESSAGE
+    }
+
+    public class Packet
+    {
+        PacketType type;
+    }
+
+    public class ChatMessagePacket : Packet
+    {
+        string message;
+
+        ChatMessagePacket(string message)
+        {
+
+        }
+    }
+
+    public class NicknamePacket : Packet
+    {
+
+    }
+}
